2c0c64fada37a1f50a4c0984d778d38fcf7455c8
User <user@hackerrank.workspace>
2024-09-15 18:52:56 +0000

    Add initial repository
    Commit by HackerRank

---
+++ src/app/textEditor/textEditor.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'text-editor',
+  templateUrl: './textEditor.component.html',
+  styleUrls: ['./textEditor.component.scss']
+})
+export class TextEditor implements OnInit {
+
+  ngOnInit() {}
+}
